32fd291ddfa8c232095809c58a6eab2cbbb6b0bb
Aayush Mishra <ayush950mishra@gmail.com>
2026-02-16 18:00:37 +0530

    Update app.js

--- back/app.js
+++ back/app.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin: "https://urgent-frontend-3jiy.onrender.com",
     credentials: true,
   })
 );
